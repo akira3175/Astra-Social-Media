393a72c1f367b96c6aacb8d2ec434944f12d7c69
Vương <vuongledz04@gmail.com>
2025-05-02 21:04:00 +0700

    fix api getAllUserLoginToday for AdminController

--- backend/src/main/java/org/example/backend/controller/AdminController.java
+++ backend/src/main/java/org/example/backend/controller/AdminController.java
@@ -230,7 +230,7 @@ public class AdminController {
     @RequireAdmin
     @GetMapping("/users/getUserLoginToday")
     public ResponseEntity<ApiResponse<Object>> getUserLoginToday() {
-        List<User> users = userService.getAllUsers().stream().filter(user -> user.getLastLogin().isAfter(LocalDateTime.now().minusDays(1))).collect(Collectors.toList());
+        List<User> users = userService.getAllUsers().stream().filter(user -> user.getLastLogin() != null && user.getLastLogin().isAfter(LocalDateTime.now().minusDays(1))).collect(Collectors.toList());
         return ResponseEntity.ok().body(ApiResponse.builder()
                 .status(200)
                 .message("Success")
